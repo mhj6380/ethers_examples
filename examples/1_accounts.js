9905399baf79d637be1eca513c996b9a599cb4f1
mhj6380 <mhj6407@gmail.com>
2022-05-06 00:59:08 +0900

    example 3 까지 진행 완료

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,14 +1,17 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const INFURA_ID = "d2627686e3b34a6db0cb54a0524bf205";
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://mainnet.infura.io/v3/${INFURA_ID}`
+);
 
-const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+const address = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e";
 
 const main = async () => {
-    const balance = await provider.getBalance(address)
-    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`)
-}
+  const balance = await provider.getBalance(address);
+  console.log(
+    `\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`
+  );
+};
 
-main()
-
+main();
--- examples/3_send_signed_transaction.js
+++ examples/3_send_signed_transaction.js
@@ -1,34 +1,56 @@
+// 송금처리 Transaction
+
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
+const INFURA_ID = "d2627686e3b34a6db0cb54a0524bf205";
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://kovan.infura.io/v3/${INFURA_ID}`
+);
 
-const account1 = '' // Your account address 1
-const account2 = '' // Your account address 2
-
-const privateKey1 = '' // Private key of account 1
-const wallet = new ethers.Wallet(privateKey1, provider)
-
+const account1 = "0xAb4E5834C33a48B0bbA585a04EeD444B48A74306"; // sender 테스트용 학구 지갑주소
+const account2 = "0x2460394330Be26326D344594B04bA8e829814e75"; // receiver 수신자 지갑주소
+const privateKey1 =
+  "84e213fc46c31f33033d0bdbdf0da2b939b3653e55c9c4387dae9867bec63e47"; // sender private key
+const wallet = new ethers.Wallet(privateKey1, provider);
 const main = async () => {
-    const senderBalanceBefore = await provider.getBalance(account1)
-    const recieverBalanceBefore = await provider.getBalance(account2)
+  // Show account 1 balance before transfer
+  const senderBalanceBefore = await provider.getBalance(account1);
+  // Show account 2 balance before transfer
+  const receiverBalanceBefore = await provider.getBalance(account2);
 
-    console.log(`\nSender balance before: ${ethers.utils.formatEther(senderBalanceBefore)}`)
-    console.log(`reciever balance before: ${ethers.utils.formatEther(recieverBalanceBefore)}\n`)
+  console.log(
+    `Sender Balance Before : ${ethers.utils.formatEther(senderBalanceBefore)}`
+  );
+  console.log(
+    `Receiver Balance Before : ${ethers.utils.formatEther(
+      receiverBalanceBefore
+    )}`
+  );
 
-    const tx = await wallet.sendTransaction({
-        to: account2,
-        value: ethers.utils.parseEther("0.025")
-    })
+  // Send Ether Trainsaction
+  const tx = await wallet.sendTransaction({
+    to: account2,
+    value: ethers.utils.parseEther("0.025"),
+  });
 
-    await tx.wait()
-    console.log(tx)
+  // Feth Transaction
+  // Trainsaction 이 끝나길 기다린다
+  await tx.wait();
+  console.log(tx);
 
-    const senderBalanceAfter = await provider.getBalance(account1)
-    const recieverBalanceAfter = await provider.getBalance(account2)
+  // Show account 1 balance after transfer
+  const senderBalanceAfter = await provider.getBalance(account1);
+  // Show account 2 balance after transfer
+  const receiverBalanceAfter = await provider.getBalance(account2);
 
-    console.log(`\nSender balance after: ${ethers.utils.formatEther(senderBalanceAfter)}`)
-    console.log(`reciever balance after: ${ethers.utils.formatEther(recieverBalanceAfter)}\n`)
-}
+  console.log(
+    `Sender Balance Before : ${ethers.utils.formatEther(senderBalanceAfter)}`
+  );
+  console.log(
+    `Receiver Balance Before : ${ethers.utils.formatEther(
+      receiverBalanceAfter
+    )}`
+  );
+};
 
-main()
+main();
